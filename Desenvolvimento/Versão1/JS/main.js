2e033ea902c1d9c9953b1690306b703a6d6af5fc
Eduardo Paixão Sehn <eduardosehn20@gmail.com>
2021-06-30 11:30:08 -0300

    Update 2 30/06/2021

--- Desenvolvimento/Versão1/JS/main.js
+++
@@ -1,9 +0,0 @@
-const opacityHeader = function(){
-    let header = document.getElementById('header_site')
-    let body = document.getElementById('body')
-    header.style.opacity = "90%"
-    let posicaoBody = body.getBoundingClientRect();
-    if (posicaoBody.y == 0){
-        header.style.opacity = "100%"
-    }
-}
