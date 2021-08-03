f407af768943285823f20499885d22def96ba135
oyyo120 <ottospi@hotmail.com>
2021-08-03 12:21:30 -0300

    Revert "Update 03/08/2021"
    This reverts commit 218bf4e02c394afd20a70c9acd9838aa6e62b575.

--- Desenvolvimento/Versão1/JS/main.js
+++ Desenvolvimento/Versão1/JS/main.js
@@ -7,15 +7,3 @@ const opacityHeader = function(){
         header.style.opacity = "100%"
     }
 }
-const calculadoraRendimento = function(){
-    let trabalho = document.getElementById('trabalho_input').value
-    let calor = document.getElementById('energia_input').value
-    let display = document.getElementById('displayCalculadoraRendimento')
-    let rendimento = trabalho / calor
-    display.textContent = rendimento.toFixed(4) + ""
-}
-const limparResultadoRendimento = function(){
-    let display = document.getElementById('displayCalculadoraRendimento')
-    display.textContent = ""
-}
-
