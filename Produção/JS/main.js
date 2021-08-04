fbdf39fc100785f62dd97af24f6577f7a690ae67
Eduardo Paixão Sehn <eduardosehn20@gmail.com>
2021-08-03 23:39:51 -0300

    update 03/08/2021

--- Produção/JS/main.js
+++ Produção/JS/main.js
@@ -1,4 +1,5 @@
-const opacityHeader = function(){
+// Faz com que o header fica semi transparente quando descer a pagina
+const opacityHeader = function(){ 
     let header = document.getElementById('header_site')
     let body = document.getElementById('body')
     header.style.opacity = "90%"
@@ -6,4 +7,35 @@ const opacityHeader = function(){
     if (posicaoBody.y == 0){
         header.style.opacity = "100%"
     }
+}
+//Executa Quando é clicado o botão calcular - Tem a Função de realizar os calculos
+const calculadoraRendimento = function(){
+    let trabalho = document.getElementById('trabalho_input').value
+    let calor = document.getElementById('energia_input').value
+    let display = document.getElementById('displayCalculadoraRendimento')
+    let simboloDeIgual = document.getElementById('simboloDeIgual').style.display = "initial"
+    let rendimento = trabalho / calor
+    console.log(trabalho)
+    console.log(calor)
+    if (trabalho && calor == 0){
+        display.textContent = 0
+    }
+    else{
+        display.textContent = rendimento.toFixed(2) 
+    }
+    
+}
+// Executa quando é clicado o botão limpar - Tem a Função de Remover o resultado e os valores digitados
+const limparResultadoRendimento = function(){
+    document.getElementById('displayCalculadoraRendimento').textContent = ""
+    document.getElementById('trabalho_input').value = 0
+    document.getElementById('energia_input').value = 0
+    document.getElementById('simboloDeIgual').style.display = "none"
+}
+
+const zerarInputTrabalho = function(){
+    document.getElementById('trabalho_input').value = undefined
+}
+const zerarInputCalor = function(){
+    document.getElementById('energia_input').value = undefined
 }
